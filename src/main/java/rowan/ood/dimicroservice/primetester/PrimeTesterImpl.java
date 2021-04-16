10afeedca59c0a1f2bb849e54d4fa43591fbfc9d
jacelynnduranceau <duranceaj3@students.rowan.edu>
2021-04-15 23:01:05 -0400

    added functions for getting information about pokemon

--- src/main/java/rowan/ood/dimicroservice/primetester/PrimeTesterImpl.java
+++ src/main/java/rowan/ood/dimicroservice/primetester/PrimeTesterImpl.java
@@ -8,6 +8,7 @@ package rowan.ood.dimicroservice.primetester;
  *
  */
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -89,6 +90,94 @@ public class PrimeTesterImpl implements PrimeTester {
         }
 
         return pokemon;
+    }
+
+    public String getPokemonHeight(int pokeID) {
+
+        String height = "";
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("http://pokeapi.co/api/v2/pokemon/" + pokeID)
+                    .get()
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            ResponseBody responseBody = response.body();
+            String json = responseBody.string();
+            JsonObject pokeInfoJson = new JsonParser().parse(json).getAsJsonObject();
+            JsonElement pokeHeight = pokeInfoJson.get("height"); //JsonArray
+            System.out.println(pokeHeight.toString());
+            height = pokeHeight.toString();
+        }
+        catch (Exception e) {
+            e.getMessage();
+        }
+
+        return height;
+    }
+
+    public String getPokemonWeight(int pokeID) {
+
+        String weight = "";
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("http://pokeapi.co/api/v2/pokemon/" + pokeID)
+                    .get()
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            ResponseBody responseBody = response.body();
+            String json = responseBody.string();
+            JsonObject pokeInfoJson = new JsonParser().parse(json).getAsJsonObject();
+            JsonElement pokeWeight = pokeInfoJson.get("weight"); //JsonArray
+            System.out.println(pokeWeight.toString());
+            weight = pokeWeight.toString();
+        }
+        catch (Exception e) {
+            e.getMessage();
+        }
+
+        return weight;
+    }
+
+    public String getPokemonType(int pokeID) {
+
+        String type = "";
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("http://pokeapi.co/api/v2/pokemon/" + pokeID)
+                    .get()
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            ResponseBody responseBody = response.body();
+            String json = responseBody.string();
+            JsonObject pokeInfoJson = new JsonParser().parse(json).getAsJsonObject();
+            JsonElement pokeType = pokeInfoJson.get("types"); //JsonArray
+            JsonElement pokeTypeEle = pokeType.getAsJsonArray().get(0);
+            JsonObject pokeTypeJson = pokeTypeEle.getAsJsonObject();
+            JsonElement pokeTypeName = pokeTypeJson.get("type");
+            JsonObject jsonType = pokeTypeName.getAsJsonObject();
+            JsonElement typeName = jsonType.get("name");
+            type = typeName.toString();
+        }
+        catch (Exception e) {
+            e.getMessage();
+        }
+
+        return type;
     }
 
 
